605d82859fb8c22dfda1e41f05aef4a0e2f02d41
mattman00000 <mattman00000@gmail.com>
2017-05-29 18:53:28 -0700

    Create forclip.sh

---
+++ forclip.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "if you see this in any fashion please open an issue to make me aware of an urgent need for the bash version"
+echo "feel free to pull-request if you port it or if I'm misremembering what's needed for a bash script"
